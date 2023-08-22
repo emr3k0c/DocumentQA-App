b039d81d586c5bbabc67e7b860eb899c71aa1741
Emre <kocemre007@gmail.com>
2023-08-22 14:57:52 +0300

    Enabled conversation chain and made program more user friendly.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from langchain import PromptTemplate, OpenAI
+from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 
 
 def main():
-    user_question = "What is Apple's profit in 2023 q1?"
+    llm = OpenAI()
     documents = []
     document_paths = ["/Users/emrekoc/Downloads/doc1.pdf", "/Users/emrekoc/Downloads/doc2.pdf", "/Users/emrekoc/Downloads/doc3.pdf"]
     for pdf in document_paths:
@@ -16,9 +18,12 @@ def main():
     documents = text_splitter.split_documents(documents)
     embeddings = OpenAIEmbeddings()
     db = Chroma.from_documents(documents, embeddings)
-    results = db.similarity_search(
-        query=user_question,
-        n_results=5
+
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=db.as_retriever(),
+        memory=memory
     )
     template = """
     You are a chat bot who loves to help people! Given the following context sections, answer the
@@ -34,21 +39,24 @@ def main():
     Answer:
     """
 
-    prompt = PromptTemplate(template=template, input_variables=["context", "users_question"])
-    prompt_text = prompt.format(context=results, users_question=user_question)
-    llm = OpenAI()
-    print(llm(prompt_text))
 
-    #This part is for different approach to have a conversation history.
-    # # memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages = True)
-    # # conversation_chain = ConversationalRetrievalChain.from_llm(
-    # #     llm = llm,
-    # #     retriever=db.as_retriever(),
-    # #     memory = memory
-    # # )
-    #
-    # response = conversation_chain({'question' : user_question})
-    # print(response)
+    while True:
+        user_question = input("You: ")  # Get user input
+
+        if user_question.lower() == "stop":
+            print("Conversation stopped.")
+            break
+
+        response = conversation_chain({'question': user_question})
+
+
+        # Format the prompt using the template
+        prompt = PromptTemplate(template=template, input_variables=["context", "users_question"])
+        prompt_text = prompt.format(context=response, users_question=user_question)
+
+        # Get the assistant's response
+        assistant_response = llm(prompt_text)
+        print("Assistant:", assistant_response)
 
 
 if __name__ == "__main__":
